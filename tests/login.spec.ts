f9c672a7ab8435baf51cd78b7a7af0c4eb4150a2
xfr333 <xfr333@gmail.com>
2025-10-30 09:43:11 +0200

    Add test case 5 from automationexercise.com

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -18,26 +18,23 @@ await test.step('Navigate to home page', async () => {
   if (await consentButton.isVisible()) {
     await consentButton.click();
   }
-  await page.click('text=Signup / Login');
-
-  await expect(page.locator('text=Login to your account')).toBeVisible();
+    await page.click('text=Signup / Login');
+    await expect(page.locator('text=Login to your account')).toBeVisible();
 });  
  
 //NEGATIVE TESTING FOR LOGIN FORM
 
 await test.step('Negative login: valid email, empty password', async () => {
-  await page.fill('[data-qa="login-email"]', config.persistent_test_user.email);
-    await page.fill('[data-qa="login-password"]', '');
-    await page.click('[data-qa="login-button"]');
-      await expect(page.locator('[data-qa="login-password"]')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
-
+  await page.fill('[data-qa="login-email"]', config.persistent_test_user.email);   await page.fill('[data-qa="login-password"]', '');
+  await page.click('[data-qa="login-button"]');
+  await expect(page.locator('[data-qa="login-password"]')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
 });
 
 await test.step('Negative login: empty email, valid password', async () => {
   await page.fill('[data-qa="login-email"]', '');
-    await page.fill('[data-qa="login-password"]', config.persistent_test_user.password);
-    await page.click('[data-qa="login-button"]');
-      await expect(page.locator('[data-qa="login-email"]')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
+  await page.fill('[data-qa="login-password"]', config.persistent_test_user.password);
+  await page.click('[data-qa="login-button"]');
+  await expect(page.locator('[data-qa="login-email"]')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
 });
 
 // await test.step('Negative login: wrong email, wrong password', async () => {
@@ -59,12 +56,11 @@ await test.step('Enter email address and password', async () => {
   const passwordField = page.locator('[data-qa="login-password"]');
   const emailValidation = await emailField.evaluate((el: HTMLInputElement) => el.validationMessage);
   const passwordValidation = await passwordField.evaluate((el: HTMLInputElement) => el.validationMessage);
-
 if (emailValidation || passwordValidation) {
     console.log(`Validation messages - Email: ${emailValidation}, Password: ${passwordValidation}`);
   const persistentUser = config.persistent_test_user;
     await page.fill('[data-qa="login-email"]', persistentUser.email);
-    await page.fill('[data-qa="login-password"]', persistentUser.password);
+      await page.fill('[data-qa="login-password"]', persistentUser.password);
 } else {
     console.log('No validation messages, proceeding with login.');
 }
@@ -76,23 +72,17 @@ await test.step('Login with persistent user', async () => {
   await page.fill('[data-qa="login-password"]', persistentUser.password);
   await page.click('[data-qa="login-button"]');
   await expect(page.locator('text=Logged in as Test')).toBeVisible();
-  console.log(`Successfully logged in as Test`);
+    console.log(`Successfully logged in as Test`);
 });
-
 
 // Verify that 'Logged in as username' is visible
 await test.step('Verify that Logged in', async () => {
-
-await expect(page.locator('text=Logged in as Test')).toBeVisible();
-  console.log(`Successfully logged in as Test`);
+  await expect(page.locator('text=Logged in as Test')).toBeVisible();
+    console.log(`Successfully logged in as Test`);
     });
-
 await test.step('Click Logout button', async () => {
   await page.click('text=Logout');
-  await expect(page.locator('[data-qa="login-email"]')).toBeVisible();
-
+   await expect(page.locator('[data-qa="login-email"]')).toBeVisible();
   await page.goto(`${config.site_url}/login`);
-
-
 });
 });
--- tests/registration.spec.ts
+++ tests/registration.spec.ts
@@ -12,34 +12,34 @@ test('Register New User', async ({ page }) => {
 //2. Navigate to url 'http://automationexercise.com'
 
 await test.step('Navigate to home page', async () => {
-await page.goto('/');
+      await page.goto('/');
 
 //3. Verify that home page is visible successfully
-await expect(page.locator('body')).toBeVisible();
-await page.locator('button', { hasText: 'Consent' }).click();
+      await expect(page.locator('body')).toBeVisible();
+      await page.locator('button', { hasText: 'Consent' }).click();
 });
 
 //4. Click on 'Signup / Login' button
 await test.step('Click on Signup / Login button', async () => {
-await page.click('text=Signup / Login');
-await expect(page.locator('body')).toBeVisible();
+      await page.click('text=Signup / Login');
+      await expect(page.locator('body')).toBeVisible();
 
 //5. Verify 'New User Signup!' is visible
 });
 
-//6.1 Enter name and email address
+//5.1 Enter name and email address
 await test.step('Enter name and email address', async () => {
       await expect(page.locator('text=New User Signup!')).toBeVisible();
       await expect(page.locator('[data-qa="signup-name"]')).toBeVisible();
       await expect(page.locator('[data-qa="signup-email"]')).toBeVisible();
       await page.getByRole('button', { name: 'Signup' }).click();
 
-//6.1 Check name validation
-            await expect(page.getByRole('textbox', { name: 'Name' })).toHaveJSProperty('validationMessage', 'Please fill out this field.');
-            await page.locator('[data-qa="signup-name"]').fill(user.username);
-//6.2 Check email validation
-                  await page.getByRole('button', { name: 'Signup' }).click();
-                  await expect(page.locator('[data-qa="signup-email"]')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
+//5.2 Check name validation
+      await expect(page.getByRole('textbox', { name: 'Name' })).toHaveJSProperty('validationMessage', 'Please fill out this field.');
+      await page.locator('[data-qa="signup-name"]').fill(user.username);
+//5.3 Ceck email validation
+      await page.getByRole('button', { name: 'Signup' }).click();
+      await expect(page.locator('[data-qa="signup-email"]')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
 
 //6.3 Enter existing user 
 await test.step('Negative login: existing user', async () => {
@@ -51,93 +51,85 @@ await test.step('Negative login: existing user', async () => {
 
 
 
-await page.locator('[data-qa="signup-name"]').fill(user.username);
-await page.locator('[data-qa="signup-email"]').fill(user.email);
+      await page.locator('[data-qa="signup-name"]').fill(user.username);
+      await page.locator('[data-qa="signup-email"]').fill(user.email);
 });
 
 //7. Click 'Signup' button
 await test.step('Click Signup button', async () => {
-await page.locator('[data-qa="signup-button"]').click();
+      await page.locator('[data-qa="signup-button"]').click();
 
 //8. Verify that 'ENTER ACCOUNT INFORMATION' is visible
-await expect(page.locator('text=Enter Account Information')).toBeVisible();
+      await expect(page.locator('text=Enter Account Information')).toBeVisible();
 });
 
 //9. Fill details: Title, Name, Email, Password, Date of birth
 await test.step('Fill details: Title, Name, Email, Password, Date of birth', async () => {
-await expect(page.locator('#id_gender1')).toBeVisible();
-await expect(page.locator('#id_gender2')).toBeVisible();
-await expect(page.locator('#name')).toBeVisible();
-await expect(page.locator('#email')).toBeVisible();
-await expect(page.locator('#password')).toBeVisible();
+      await expect(page.locator('#id_gender1')).toBeVisible();
+      await expect(page.locator('#id_gender2')).toBeVisible();
+      await expect(page.locator('#name')).toBeVisible();
+      await expect(page.locator('#email')).toBeVisible();
+      await expect(page.locator('#password')).toBeVisible();
 
-await page.locator('#id_gender1').check();
-await expect(page.locator('#name')).toHaveValue(user.username);
-await expect(page.locator('#email')).toHaveValue(user.email);
-await page.locator('#password').fill('Test1234');
-await page.locator('#days').selectOption('10');
-await page.locator('#months').selectOption('5');
-await page.locator('#years').selectOption('1990');     
+      await page.locator('#id_gender1').check();
+      await expect(page.locator('#name')).toHaveValue(user.username);
+      await expect(page.locator('#email')).toHaveValue(user.email);
+      await page.locator('#password').fill('Test1234');
+      await page.locator('#days').selectOption('10');
+      await page.locator('#months').selectOption('5');
+      await page.locator('#years').selectOption('1990');     
 });
 
 //9.0 Check adress infromation validation
 await test.step('Check adress infromation validation', async () => {
-await expect(page.locator('#first_name')).toBeVisible();
-await expect(page.locator('#last_name')).toBeVisible();
-await expect(page.locator('#company')).toBeVisible();
-await expect(page.locator('#address1')).toBeVisible();
-await expect(page.locator('#address2')).toBeVisible();
-await expect(page.locator('#country')).toBeVisible();
-await expect(page.locator('#state')).toBeVisible();
-await expect(page.locator('#city')).toBeVisible();
-await expect(page.locator('#zipcode')).toBeVisible();
-await expect(page.locator('#mobile_number')).toBeVisible();
+      await expect(page.locator('#first_name')).toBeVisible();
+      await expect(page.locator('#last_name')).toBeVisible();
+      await expect(page.locator('#company')).toBeVisible();
+      await expect(page.locator('#address1')).toBeVisible();
+      await expect(page.locator('#address2')).toBeVisible();
+      await expect(page.locator('#country')).toBeVisible();
+      await expect(page.locator('#state')).toBeVisible();
+      await expect(page.locator('#city')).toBeVisible();
+      await expect(page.locator('#zipcode')).toBeVisible();
+      await expect(page.locator('#mobile_number')).toBeVisible();
 });
 
 //9.1 Check adress validation
 await test.step('Check adress validation', async () => {
-await page.getByRole('button', { name: 'Create Account' }).click();
-await expect(page.locator('#first_name')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
-await page.locator('#first_name').fill('Georgi');
-
-
-await page.getByRole('button', { name: 'Create Account' }).click();
-await expect(page.locator('#last_name')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
-await page.locator('#last_name').fill('Atanasov');
-
-await page.getByRole('button', { name: 'Create Account' }).click();
-await expect(page.locator('#address1')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
-await page.locator('#address1').fill('Some Address 123');
+      await page.getByRole('button', { name: 'Create Account' }).click();
+      await expect(page.locator('#first_name')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
+      await page.locator('#first_name').fill('Georgi');
+      await page.getByRole('button', { name: 'Create Account' }).click();
+      await expect(page.locator('#last_name')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
+      await page.locator('#last_name').fill('Atanasov');
+      await page.getByRole('button', { name: 'Create Account' }).click();
+      await expect(page.locator('#address1')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
+      await page.locator('#address1').fill('Some Address 123');
 });
 
 //await page.getByRole('button', { name: 'Create Account' }).click();
 //await expect(page.locator('#country')).toHaveJSProperty('validationMessage', 'Please select an item in the list.');
 await test.step('Select country', async () => {
-await page.locator('#country').selectOption('Canada');
-
-await page.getByRole('button', { name: 'Create Account' }).click();
-await expect(page.locator('#state')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
-await page.locator('#state').fill('Some State');
-
-await page.getByRole('button', { name: 'Create Account' }).click();
-await expect(page.locator('#city')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
-await page.locator('#city').fill('Some City');
-
-await page.getByRole('button', { name: 'Create Account' }).click();
-await expect(page.locator('#zipcode')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
-await page.locator('#zipcode').fill('12345');
-
-await page.getByRole('button', { name: 'Create Account' }).click();
-await expect(page.locator('#mobile_number')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
-await page.locator('#mobile_number').fill('+1234567890');
+      await page.locator('#country').selectOption('Canada');
+      await page.getByRole('button', { name: 'Create Account' }).click();
+      await expect(page.locator('#state')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
+      await page.locator('#state').fill('Some State');
+      await page.getByRole('button', { name: 'Create Account' }).click();
+      await expect(page.locator('#city')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
+      await page.locator('#city').fill('Some City');
+      await page.getByRole('button', { name: 'Create Account' }).click();
+      await expect(page.locator('#zipcode')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
+      await page.locator('#zipcode').fill('12345');
+      await page.getByRole('button', { name: 'Create Account' }).click();
+      await expect(page.locator('#mobile_number')).toHaveJSProperty('validationMessage', 'Please fill out this field.');
+      await page.locator('#mobile_number').fill('+1234567890');
 });
 
 //10. Select checkbox 'Sign up for our newsletter!'
 await test.step('Select checkbox Sign up for our newsletter!', async () => {
-await page.locator('#newsletter').check();
-
+      await page.locator('#newsletter').check();
 //11. Select checkbox 'Receive special offers from our partners!'
-await page.locator('#optin').check();
+      await page.locator('#optin').check();
 });
 
 //12. Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
@@ -146,26 +138,21 @@ await page.locator('#optin').check();
 
 //13. Click 'Create Account button'
 await test.step('Click Create Account button', async () => {
-await page.getByRole('button', { name: 'Create Account' }).click();
-
+      await page.getByRole('button', { name: 'Create Account' }).click();
 //14. Verify that 'ACCOUNT CREATED!' is visible
-await expect(page.locator('text=Account Created!')).toBeVisible();
-
+       await expect(page.locator('text=Account Created!')).toBeVisible();
 //15. Click 'Continue' button
-await page.locator('[data-qa="continue-button"]').click();
-
+      await page.locator('[data-qa="continue-button"]').click();
 //16. Verify that 'Logged in as username' is visible
-await expect(page.locator(`text=Logged in as ${user.username}`)).toBeVisible();
+      await expect(page.locator(`text=Logged in as ${user.username}`)).toBeVisible();
 });
-
 
 //17. Click 'Delete Account' button
 await test.step('Click Delete Account button', async () => {
-await page.getByRole('link', { name: 'Delete Account' }).click();
-
+      await page.getByRole('link', { name: 'Delete Account' }).click();
 //18. Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
-await expect(page.locator('text=Account Deleted!')).toBeVisible();
-await page.locator('[data-qa=continue-button]').click();
+      await expect(page.locator('text=Account Deleted!')).toBeVisible();
+      await page.locator('[data-qa=continue-button]').click();
 });
 
 });
